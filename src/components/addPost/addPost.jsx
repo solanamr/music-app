67a99b617a54bd6aa16e5a8a6a8116ac64c9114d
noeerolli <noeliarolli82@gmail.com>
2023-08-26 21:49:19 -0300

    addPost

--- src/components/addPost/addPost.jsx
+++ src/components/addPost/addPost.jsx
@@ -6,17 +6,6 @@ import instrumentos from "../../assets/instrumentos.jpg";
 
 const AddPost = () => {
 
-    // function getCurrentDateTime() {
-    //     const now = new Date();
-    //     const year = now.getFullYear();
-    //     const month = String(now.getMonth() + 1).padStart(2, "0");
-    //     const day = String(now.getDate()).padStart(2, "0");
-    //     const hours = String(now.getHours()).padStart(2, "0");
-    //     const minutes = String(now.getMinutes()).padStart(2, "0");
-    //     return `${year}-${month}-${day}T${hours}:${minutes}`;
-    // }
-
-
     const {
         register,
         handleSubmit,
@@ -31,39 +20,25 @@ const AddPost = () => {
         <div>
             <Navbar/>
         
-            <div className="h-screen flex justify-center bg-lightBlue">
+            <div className="main-body items-center flex justify-center bg-lightBlue w-full h-screen">
 
                 
 
                 <form className="flex-row" onSubmit={handleSubmit(onSubmit)}>
                     
-                    <div className="card-container bg-white flex card-container rounded-xl py-4 ">
+                    <div className="card-container self-center bg-white rounded-xl py-4 px-6 md:flex md:flex-row md:space-x-6 ">
                         
-                        <div className="user-image shadow-2xl p-4 flex mx-7 w-96 h-96" >
-                            <img className="object-cover w-fit-content " src={instrumentos}></img>    
+                        <div className="user-image shadow-2xl p-4 flex md:w-96 md:h-96 ml:w-full" >
+                            <img className="object-cover w-fit-content " src={instrumentos} alt="Instrumentos musicales"></img>    
                         </div>
-                        <div className="flex-row space-y-4 inputs">
+                        <div className="inputs-container flex-row space-y-4 p-4">
                         <div className="text-center">
                         
                             <h1 className="text-black text-3xl font-bold inline-block">New Post</h1>
                         </div> 
-                       
-                        {/* <div className="text-right">
-                            <input
-                                name="date"
-                                type="datetime-local"
-                                defaultValue={getCurrentDateTime()}
-                                {...register("date", {                                 
-                                    required:{
-                                        value:true 
-                                    },
-                                   
-                                 })}
-                            />    
-                        </div>       */}
-                       
-                        <div className="text-black">
-                                <input type="text" className="mx-2 border border-purple-600 rounded-md  text-black p-1 w-5/6"  name="title" placeholder="Title"
+
+                        <div className="title-field text-black flex justify-center">
+                                <input type="text" className="w-full border border-purple-600 rounded-md  text-black p-1"  name="title" placeholder="Title"
                                 {...register("title", {                                 
                                 required:{
                                     value:true,
@@ -77,51 +52,47 @@ const AddPost = () => {
                                 />
                                 {errors.title && <span className='text-xs italic mx-7'>{errors.title.message}</span>}
                             </div>
-                            <div className="flex">
-                                <select name="categoria" className="rounded-md px-4 py-2 mx-2 border border-purple-600 " 
-                                {...register("categoria",
-                                { required: {
-                                    value:true,
-                                    message:"Seleccione una categoria"
-                                
-                                }
-                                })}>
+                            <div className="cat-file-container flex">
+                                    <select name="categoria" className="rounded-md px-2 py-2 border border-purple-600 my-1" 
+                                    {...register("categoria",
+                                    { required: {
+                                        value:true,
+                                        message:"Seleccione una categoria"
+                                    
+                                    }
+                                    })}>
+                                                
+                                        <option value=""> Categorías</option>
+                                        <option value="1">Reseñas</option>                                  
+                                        <option value="2">Entrevista a Artistas</option>
+                                        <option value="3">Géneros Musicales</option>
+                                        <option value="4">Descubriendo Nuevos Talentos</option>
+                                        <option value="5">Historias de Bandas y Artistas Famosos</option>
+                                        <option value="6">Conciertos y Festivales</option>
+                                        <option value="7">Tutoriales</option>
+                                        <option value="8">Noticias y Tendencias de la Industria</option>
+                                        <option value="9">Historia de la Musica</option>
+                                        <option value="10">Instrumentos Musicales</option>
                                             
-<<<<<<< HEAD
-                                    <option value="" defaultValue={"Categoría"}> Categorías</option>
-=======
-                                    <option value=""> Categorías</option>
->>>>>>> eef11830361ff86720d3fb91c64a72bdec08a4c3
-                                    <option value="1">Reseñas</option>                                  
-                                    <option value="2">Entrevista a Artistas</option>
-                                    <option value="3">Géneros Musicales</option>
-                                    <option value="4">Descubriendo Nuevos Talentos</option>
-                                    <option value="5">Historias de Bandas y Artistas Famosos</option>
-                                    <option value="6">Conciertos y Festivales</option>
-                                    <option value="7">Tutoriales</option>
-                                    <option value="8">Noticias y Tendencias de la Industria</option>
-                                    <option value="9">Historia de la Musica</option>
-                                    <option value="10">Instrumentos Musicales</option>
-                                        
-                                            
-                                </select>
-                                {errors.categoria && <span className='text-xs italic mx-7'>{errors.categoria.message}</span>}
+                                                
+                                    </select>
+                                    {errors.categoria && <span className='text-xs italic mx-7'>{errors.categoria.message}</span>}
+                                <div className="file-container mt-2 md:mt-0 md:ml-2 flex">
+                                    <label htmlFor="file"  className="md:w-24" > Agregá una imagen:</label>
                                 
-
-                                <label htmlFor="file"  className="ml-6 w-24" > Agregá una imagen:</label>
-
-                                <input  className="mr-8" type="file"  name="file" accept="image/png, image/jpeg"
-                                 {...register("file", {                                 
-                                    required:{
-                                        value:false,
-                                    },
-                                 
-                                    })}
-                                />
+                                    <input  className="" type="file"  name="file" accept="image/png, image/jpeg"
+                                    {...register("file", {                                 
+                                        required:{
+                                            value:false,
+                                        },
+                                    
+                                        })}
+                                    />
+                                </div>
                             </div>
                            
-                            <div>
-                                <textarea className="mx-2 border border-purple-600 rounded-md  text-black w-5/6 p-4" type="text" name="post" placeholder="Post"  rows="7"
+                            <div className="text-area-container">
+                                <textarea className=" border border-purple-600 rounded-md  text-black w-full p-4" type="text" name="post" placeholder="Post"  rows="7"
                                 {...register("post",{
                                     required: true,
                                     message: "Debe completar este campo"
@@ -129,8 +100,9 @@ const AddPost = () => {
                                 ></textarea>
                                 {errors.post && <span className='text-xs italic mx-7'>{errors.post.message}</span>}
                             </div>
-                            
-                            <button type="submit" className="text-white bg-blue px-4 py-1 rounded-md mt-7 ml-[46%]">Add Post</button>
+                            <div className="button-add flex justify-center">
+                                <button type="submit" className="text-white bg-blue px-4 py-1 rounded-md mt-7">Add Post</button>
+                            </div>
                         </div>
 
                     </div>
